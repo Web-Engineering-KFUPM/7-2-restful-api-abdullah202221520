6e704f96e49a9a700603ccbc166760a0603437db
Abdullah <sheheween1518@gmail.com>
2026-04-15 11:27:28 +0300

    feat: configure dotenv to load environment variables in server.js

--- 7-2-restful-api/server/server.js
+++ 7-2-restful-api/server/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 
 // import dotenv and load environment variables from .env
-
+import dotenv from "dotenv";
+dotenv.config();
 
 import { connectDB } from "./db.js";
 import { Song } from "./models/song.model.js";
@@ -10,15 +11,23 @@ import { Song } from "./models/song.model.js";
 const app = express();
 const PORT = process.env.PORT || 5174;
 
-app.use(cors());              
+app.use(cors());
 app.use(express.json());
 
 await connectDB(process.env.MONGO_URL);
 
 // api/songs (Read all songs)
-
+app.get("/api/songs", async (req, res) => {
+    const songs = await Song.find();
+    res.json(songs);
+});
 
 // api/songs (Insert song)
+app.post("/api/songs", async (req, res) => {
+    const song = new Song(req.body);
+    await song.save();
+    res.json(song);
+});
 
 // /api/songs/:id (Update song)
 
